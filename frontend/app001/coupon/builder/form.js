1b12fb3d917d5d0a25a9308e3dc7c31bb5d2fbfb
Yasar Bayar <yasarbayar@gmail.com>
2016-10-05 14:30:33 +0300

    nexus refs #2228 @8h15m: Formlara ?zel yard?m/a??klama b?l?mleri olu?turulacak --

--- frontend/app001/coupon/builder/form.js
+++ frontend/app001/coupon/builder/form.js
@@ -1,8 +1,9 @@
 /**
  * Created by burak on 26.09.2016.
  */
-_.merge(couponBuilder.form,{
+_.merge(couponBuilder.form, {
     title       : 'Coupon',
+    showHelp    : true,
     beforeCreate: function ($injector, $scope, builder) {
         var vm = this;
         //vm.stay = true;
