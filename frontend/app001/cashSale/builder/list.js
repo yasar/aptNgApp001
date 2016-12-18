38c874f44332379b3d53bfdcb9e64781cb483511
yasar <yasar@localhost>
2016-12-18 16:37:02 +0300

    nexus refs #1946 @2h07m: Genel Kontrol ve D?zeltmeler --

--- frontend/app001/cashSale/builder/list.js
+++ frontend/app001/cashSale/builder/list.js
@@ -1,8 +1,8 @@
 /**
  * Created by burak on 26.09.2016.
  */
-_.merge(cashSaleBuilder.list,{
+_.merge(cashSaleBuilder.list, {
     controller: function ($injector, $scope, builder) {
-
+        alert(1);
     }
 });
--- frontend/app001/client/delete/builder/layout.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by burak on 26.09.2016.
- */
-_.merge(clientBuilder.layout,{
-    templConfig: {
-        showSidebarLeft: true
-    }
-});
