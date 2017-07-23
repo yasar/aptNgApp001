23a3d607eaa4d0aa653127e5d9d251f47af7ac65
Yasar Bayar <yasarbayar@gmail.com>
2017-07-23 19:32:18 +0300

    2521 ?r?n ekleme @1h0m

--- backend/app001/price/PriceRecord.php
+++ backend/app001/price/PriceRecord.php
@@ -9,28 +9,35 @@
 namespace BYRWEB\app001\price;
 
 
+
+
+use BYRWEB\app998\tax\Tax;
 use BYRWEB\base\ADbRecord;
 use BYRWEB\base\Exception;
 use BYRWEB\base\SqlObject;
-use BYRWEB\lup\tax\Tax;
-use BYRWEB\lup\tax\TaxRecord;
+
+
+//use BYRWEB\lup\tax\Tax;
+//use BYRWEB\lup\tax\TaxRecord;
 
 class PriceRecord extends ADbRecord
 {
-    
-    
-    public $price_id, $price, $currency_id, $currency_rate,
-        $is_tax_included, $tax_id, $discount_type, $discount_percentage,
-        $discount_amount, $base_price, $tax, $taxed_price, $timestamp, $__is_incomplete;
-    
-    
-    public function __construct()
-    {
-        parent::__construct();
-        
-        $this->setTableName('`app001.price`');
-        $this->setPrimaryKey('price_id');
-        $this->setSqlQuery(new SqlObject("SELECT
+	
+	
+	public $price_id, $price, $currency_id, $currency_rate,
+		$is_tax_included, $tax_id, $discount_type, $discount_percentage,
+		$discount_amount, $base_price, $tax, $taxed_price, $timestamp, $__is_incomplete;
+	
+	
+	
+	public
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->setTableName('`app001.price`');
+		$this->setPrimaryKey('price_id');
+		$this->setSqlQuery(new SqlObject('SELECT
 	_.*
 FROM
 	(
@@ -44,32 +51,38 @@ FROM
 	`app001.price` AS p
 LEFT JOIN lup_currency AS lc ON lc.currency_id = p.currency_id
 LEFT JOIN lup_tax AS lt ON lt.tax_id = p.tax_id
-	) AS _"), SqlObject::SQL_FOR_FIND);
-    
-    }
-    
-    public function analyseTax()
-    {
-        if (!$this->tax_id) {
-            throw new Exception('Price needs a tax_id defined.');
-        }
-        
-        $taxRecord = Tax::getBy(['tax_id' => $this->tax_id]);
-        
-        if (!$taxRecord) {
-            throw new Exception('Price can not get/found the tax record.');
-        }
-        
-        if ($this->is_tax_included) {
-            $this->base_price  = $this->price / ((1 + $taxRecord->tax_percentage / 100));
-            $this->taxed_price = $this->price;
-            $this->tax         = $this->taxed_price - $this->base_price;
-        } else {
-            $this->taxed_price = $this->price + (($this->price * $taxRecord->tax_percentage) / 100);
-            $this->base_price  = $this->price;
-            $this->tax         = $this->taxed_price - $this->base_price;
-        }
-    }
-    
+	) AS _'), SqlObject::SQL_FOR_FIND);
+	
+	}
+	
+	
+	
+	public
+	function analyseTax()
+	{
+		if (!$this->tax_id) {
+			throw new Exception('Price needs a tax_id defined.');
+		}
+		
+		$this->db->disableCheckRight();
+		$taxRecord = Tax::getBy(['tax_id' => $this->tax_id]);
+		$this->db->enableCheckRight();
+		
+		if (!$taxRecord) {
+			throw new Exception('Price can not get/found the tax record.');
+		}
+		
+		if ($this->is_tax_included) {
+			$this->base_price  = $this->price / ((1 + $taxRecord->tax_percentage / 100));
+			$this->taxed_price = $this->price;
+			$this->tax         = $this->taxed_price - $this->base_price;
+		}
+		else {
+			$this->taxed_price = $this->price + (($this->price * $taxRecord->tax_percentage) / 100);
+			$this->base_price  = $this->price;
+			$this->tax         = $this->taxed_price - $this->base_price;
+		}
+	}
+	
 }
 
