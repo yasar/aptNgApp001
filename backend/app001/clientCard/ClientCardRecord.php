d4a2a339eb3a568409fef164bcdd76f3af1b2343
yasar <yasar@localhost>
2017-07-24 03:13:01 +0300

    nexus refs #2462 @1h17m: M??teri kart? ekleme b?l?m? g?r?nm?yor --

--- backend/app001/clientCard/ClientCardRecord.php
+++ backend/app001/clientCard/ClientCardRecord.php
@@ -9,6 +9,8 @@
 namespace BYRWEB\app001\clientCard;
 
 
+
+
 use BYRWEB\app999\entity\EntityRecord;
 use BYRWEB\app999\person\PersonRecord;
 use BYRWEB\base\ADbRecord;
@@ -17,16 +19,19 @@ use BYRWEB\base\SqlObject;
 
 class ClientCardRecord extends ADbRecord
 {
-    public $client_card_id, $enterprise_id, $client_id, $card_id, $is_primary, $password;
-    public $card_no;
-
-    public function __construct()
-    {
-        parent::__construct();
-        
-        $this->setTableName('client_card');
-        $this->setPrimaryKey('client_card_id');
-        $this->setSqlQuery(new SqlObject("select * from (
+	public $client_card_id, $enterprise_id, $client_id, $card_id, $is_primary, $password;
+	public                                                                     $card_no;
+	
+	
+	
+	public
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->setTableName('`app001.client_card`');
+		$this->setPrimaryKey('client_card_id');
+		$this->setSqlQuery(new SqlObject('SELECT * FROM (
         SELECT
         cc.client_card_id,
         cc.client_id,
@@ -35,10 +40,10 @@ class ClientCardRecord extends ADbRecord
         cc.enterprise_id,
         c.card_no
         FROM
-        client_card AS cc
-        INNER JOIN card AS c ON c.card_id = cc.card_id
-        ) _"),SqlObject::SQL_FOR_FIND);
-    }
-
+        `app001.client_card` AS cc
+        INNER JOIN `app001.card` AS c ON c.card_id = cc.card_id
+        ) _'), SqlObject::SQL_FOR_FIND);
+	}
+	
 }
 
